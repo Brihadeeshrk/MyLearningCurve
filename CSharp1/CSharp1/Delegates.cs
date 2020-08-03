89679483401e856f70c9bd65e3339e7cbcfefa30
briha <briha@test.com>
2020-08-03 22:21:17 +0530

    delegates added

---
+++ CSharp1/CSharp1/Delegates.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CSharp1
+{
+    public class Delegates
+    {
+        void main()
+        {
+            //Blank Class created
+        }
+    }
+
+    public class Photo
+    {
+        public static Photo Load(string path)
+        {
+            return new Photo();
+        }
+
+        public void Save()
+        {
+            //Implement Saving Logic
+        }
+    }
+
+    public class PhotoFilters
+    {
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Apply brightness");
+        }
+
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Apply contrast");
+        }
+
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("Resize photo");
+        }
+    }
+
+    public class PhotoProcessor
+    {
+        public void Process(string path, Action<Photo> filterHandler)
+        {
+            var photo = Photo.Load(path);
+
+            filterHandler(photo);
+
+            photo.Save();
+        }
+    }
+}
--- CSharp1/CSharp1/Program.cs
+++ CSharp1/CSharp1/Program.cs
@@ -6,6 +6,19 @@ namespace CSharp1
     {
         static void Main(string[] args)
         {
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            Action<Photo> filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+
+            processor.Process("photo.jpg", filterHandler);
+        }
+
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Apply RemoveRedEye");
+
         }
     }
 }
