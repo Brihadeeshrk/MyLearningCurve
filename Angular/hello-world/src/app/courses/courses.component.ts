264d770e739cd1f20ed9709899451820fffa3150
briha <briha@test.com>
2020-08-10 19:33:17 +0530

    eventhandling and pipes

--- Angular/hello-world/src/app/courses/courses.component.ts
+++ Angular/hello-world/src/app/courses/courses.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent {
-  title="List of Courses";
-  
-  onSave(){
-    console.log("button clicked")
-  }
+  text = `
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae lacus in ligula fringilla dignissim a eu nisl. Sed sed maximus mi. Nullam vel nunc nec purus rhoncus tristique. Cras imperdiet aliquam mollis. Pellentesque malesuada, diam quis rutrum faucibus, sem ante tincidunt purus, luctus lobortis leo ipsum vitae sem. Integer rutrum eros posuere vestibulum finibus. Donec magna diam, vulputate vel orci vitae, fringilla placerat nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas quis turpis et ante molestie tincidunt. Integer scelerisque nisi vel porta faucibus. Nam malesuada scelerisque ullamcorper. Praesent facilisis fringilla bibendum.
+
+  Praesent bibendum scelerisque lectus, a dignissim lorem ultrices eu. Aliquam erat volutpat. Etiam scelerisque purus augue, quis tristique lorem pharetra sit amet. In congue dignissim sapien, ornare maximus eros congue nec. Donec tincidunt arcu quis purus hendrerit ultricies. Etiam vitae sem consequat arcu congue consequat. Cras eget fringilla metus. 
+          `
 }
